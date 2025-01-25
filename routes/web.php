46ccbdb942f93aa7abb33b5232589ecfb6e454c4
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-01-25 19:04:39 +0600

    laravel 11 environment setup

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('register');
+    return view('welcome');
 });
-
-
-
-Route::post('/register', 'userController@store');
-Route::get('/search', 'userController@index');
-Route::get('/search/info', 'userController@info')->name('search.info');
-
